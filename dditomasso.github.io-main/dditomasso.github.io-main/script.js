7c333b435cd85cde0d1aa80583ccf3932e0bf239
Dani <72038450+dditomasso@users.noreply.github.com>
2024-12-17 21:49:19 -0500

    Add files via upload

---
+++ dditomasso.github.io-main/dditomasso.github.io-main/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.getElementById("animatedText").classList.add("visible");
+    }, 6000);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.getElementById("paragraph").classList.add("visible");
+    }, 6000);
+});
+
